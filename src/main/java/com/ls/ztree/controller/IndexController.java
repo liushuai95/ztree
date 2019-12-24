63bf0df2cc2fc419c57def57d81c5b654f0a49d4
lss <364477308@qq.com>
2019-12-24 17:08:17 +0800

    ztree-demo

--- src/main/java/com/ls/ztree/controller/IndexController.java
+++ src/main/java/com/ls/ztree/controller/IndexController.java
@@ -3,15 +3,19 @@ package com.ls.ztree.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/hello")
 public class IndexController {
 
     @GetMapping
-    public String index(){
+    public ModelAndView index(){
         //ModelAndView mv = new ModelAndView();
         //mv.setViewName("index");
-        return "demo";
+        System.out.println("3333");
+        return new ModelAndView("demo");
     }
+
+
 }
